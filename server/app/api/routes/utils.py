5832ea8ac5e6dea3473e72caa23af0739be04609
Abhishek Kashyap <abhik5567@gmail.com>
2025-08-24 14:57:05 +0530

    update/server:  add a client and database attribute in FastApi app

--- server/app/api/routes/utils.py
+++ server/app/api/routes/utils.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, Response
 import time
+import uuid
 
 router = APIRouter()
 from fastapi.requests import Request
 
 
 @router.get("/ping", response_class=Response)
-async def ping(r: Request):
-    data = {"name": "Abhishek"}
-    k = r.app.db
-    start_time = time.monotonic()
-    collection = k["form_builder"]
+async def ping(req: Request):
+    data = {"name": "Abhishek", "_id": str(uuid.uuid4())}
+    start_time = time.time()
+    collection = req.app.mongodb
     await collection["test"].insert_one(data)
-    end_time = time.monotonic()
+    end_time = time.time()
     duration = (end_time - start_time) * 1000
     return Response(f"Mongo ping: {duration:.2f} milliseconds")
--- server/app/db/db.py
+++ server/app/db/db.py
@@ -1,13 +1,14 @@
 from pymongo import AsyncMongoClient
 from fastapi import FastAPI
 
-client = AsyncMongoClient()
-
 
 async def connect(app: FastAPI) -> None:
     client = AsyncMongoClient("mongodb://127.0.0.1:27017")
-    app.db = client
+    app.mongo_client = client
+    app.mongodb = client.get_database("form_builder")
     print("Database connected")
 
-async def disconnect(app:FastAPI) -> None:
-    await app.db.disconnect()
+
+async def disconnect(app: FastAPI) -> None:
+    await app.mongo_client.disconnect()
+    print("Database disconnected")
