f20de0d9136bdf01e748c930dbf50371661547fb
Abhishek Kashyap <abhik5567@gmail.com>
2025-08-28 11:28:09 +0530

    feat/server(auth): add basic authentication with plain text password

--- server/app/api/main.py
+++ server/app/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.routes import home, utils, users
+from api.routes import home, utils, users, authentication
 
 
 api_router = APIRouter()
@@ -7,3 +7,4 @@ api_router = APIRouter()
 api_router.include_router(home.router)
 api_router.include_router(utils.router)
 api_router.include_router(users.router)
+api_router.include_router(authentication.router)
---
+++ server/app/api/routes/authentication.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Request
+from pydantic import EmailStr, BaseModel, Field
+from datetime import timedelta
+from core.security import create_access_token
+
+router = APIRouter(tags=["authentication"])
+
+
+class UserAuth(BaseModel):
+    email: EmailStr = Field(..., examples=["test@example.com"])
+    password: str = Field(
+        ...,
+    )
+
+
+@router.post("/login")
+async def login(req: Request, user: UserAuth):
+    email = user.email
+    password = user.password
+    users = req.app.mongodb["users"]
+    user = await users.find_one({"email": email, "password": password})
+
+    if not user:
+        return {"message": "Invalid email or password"}, 401
+
+    access_token = create_access_token(
+        subject=email, expires_delta=timedelta(minutes=15)
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
