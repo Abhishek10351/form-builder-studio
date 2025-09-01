6e7c37ee06b9521ad6e8421a5a8ed8784df67bc9
Abhishek Kashyap <abhik5567@gmail.com>
2025-09-01 11:38:37 +0530

    feat: add cors middleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from db import db
 from api.main import api_router
@@ -14,5 +15,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(middleware.AuthMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
