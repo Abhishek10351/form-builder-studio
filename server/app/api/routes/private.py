5a36554b8b2425f05873056bca1f6c80ac7635e9
Abhishek Kashyap <abhik5567@gmail.com>
2025-10-11 11:48:09 +0530

    fix/server: fix private routes

--- server/app/api/routes/private.py
+++ server/app/api/routes/private.py
@@ -1,22 +1,34 @@
-from fastapi import Request, status
-from fastapi.routing import APIRouter
+from fastapi import Request, Response, status, APIRouter
 from models import User
+from core.security import get_password_hash
+import json
+
 
 router = APIRouter(tags=["private"])
 
 
 # create a superuser
-@router.post("/create-superuser", response_model=User, status_code=status.HTTP_201_CREATED)
-async def create_superuser(req: Request):
-    mongo = req.app.mongodb
-    user_data = await req.json()
-    user = User(**user_data)
-    user.is_superuser = True
-    await mongo.users.insert_one(user.model_dump())
-    return user
+@router.post(
+    "/create-superuser", response_model=User, status_code=status.HTTP_201_CREATED
+)
+async def create_superuser(req: Request, user: User):
+    try:
+        collection = req.app.mongodb["users"]
+        user.password = get_password_hash(user.password)
+        user.is_superuser = True
+        result = await collection.insert_one(user.model_dump())
+        inserted_user = await collection.find_one({"_id": result.inserted_id})
+        return User(**inserted_user)
+    except:
+        return Response(
+            status_code=500,
+            content=json.dumps({"message": "Internal server error"}),
+            media_type="application/json",
+        )
+
 
 # list all users
-@router.get("/", response_model=list[User], status_code=status.HTTP_200_OK)
+@router.get("/users", response_model=list[User], status_code=status.HTTP_200_OK)
 async def get_all_users(req: Request):
     collection = req.app.mongodb["users"]
     users = await collection.find().to_list(100)
