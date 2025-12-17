c6b30a57aae9a16da9983def353cc25f2323b3c6
Abhishek Kashyap <abhik5567@gmail.com>
2025-12-17 20:32:33 +0530

    refactor(server): use modular func for auth middleware

--- server/app/middleware/auth.py
+++ server/app/middleware/auth.py
@@ -10,21 +10,26 @@ class AuthMiddleware(BaseHTTPMiddleware):
         skip_paths = ["/login", "/users/create", "/docs", "/redoc", "/openapi.json"]
         if request.url.path in skip_paths:
             return await call_next(request)
-        auth_token = request.headers.get("Authorization")
-        user = None
-
-        if auth_token and auth_token.startswith("Bearer "):
-            token = auth_token.split(" ")[1]
-            mongodb = request.app.mongodb
-            payload = verify_token(token)
-            if payload:
-                user = await mongodb["users"].find_one({"email": payload["sub"]})
-                if user:
-                    user = User(**user)
-                    user.password = None
-
+        user = await self.get_user_from_jwt(request)
         # Attach the user to the request state
         request.state.user = user
 
         response = await call_next(request)
         return response
+    
+    def get_user_from_jwt(self, request: Request):
+        auth_token = request.headers.get("Authorization")
+        if not auth_token or not auth_token.startswith("Bearer "):
+            return None
+        token = auth_token.split(" ")[1]
+        payload = verify_token(token)
+        return self.get_user(payload)
+    
+    def get_user(self, payload):
+        mongodb = request.app.mongodb
+        user_data = mongodb["users"].find_one({"email": payload["sub"]})
+        if user_data:
+            user = User(**user_data)
+            user.password = None
+            return user
+        return None
