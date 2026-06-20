459363f5395b31ef41d2d41cef5cfc4c40b4fd1d
Abhishek Kashyap <abhik5567@gmail.com>
2026-06-20 12:35:57 +0530

    fix!: fix next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,11 @@
-import type { NextConfig } from "next";
+import  { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+     
+    allowedDevOrigins: [
+        '127.0.0.1',
+        "192.168.*.*"
+    ],
     images: {
         remotePatterns: [
             {
